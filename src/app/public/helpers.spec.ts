05602bfe04aeb201f375a8037d83f8a188a465ff
Alex Kingman <alex.kingman@blackbaud.com>
2018-11-16 14:42:17 -0500

    renamed spec file, added helper methods

--- src/app/public/helpers.spec.ts
+++ src/app/public/helpers.spec.ts
@@ -1,5 +1,18 @@
 import {
-  getData
+  EventEmitter
+} from '@angular/core';
+
+import {
+  Observable
+} from 'rxjs/Observable';
+
+import {
+  Subject
+} from 'rxjs/Subject';
+
+import {
+  getData,
+  isObservable
 } from './helpers';
 
 describe('list helpers', () => {
@@ -76,5 +89,19 @@ describe('list helpers', () => {
     };
      let result = getData(data, undefined);
     expect(result).toBeUndefined();
+  });
+});
+
+describe('list helpers', () => {
+  it('should check if an object is an observable', () => {
+    const eventEmitter = new EventEmitter<void>();
+    const observable = new Observable<void>();
+    const subject = new Subject<void>();
+
+    expect(isObservable(eventEmitter)).toEqual(true);
+    expect(isObservable(observable)).toEqual(true);
+    expect(isObservable(subject)).toEqual(true);
+    expect(isObservable({})).toEqual(false);
+    expect(isObservable('foobar')).toEqual(false);
   });
 });
